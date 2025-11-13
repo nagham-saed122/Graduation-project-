a88f3968746789fcf3fcfa07fb8d2456067a98f8
KareemFareed06 <kareem.fahme06@eng-st.cu.edu.eg>
2025-11-13 20:19:37 +0200

    Update TestRunner.java

---
+++ OpenCart-Automation-Framework/runners/TestRunner.java
@@ -0,0 +1,14 @@
+package runners;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "features",
+    glue = "stepDefinitions",
+    plugin = {"pretty", "html:target/cucumber-reports.html"},
+    monochrome = true
+)
+public class TestRunner {}

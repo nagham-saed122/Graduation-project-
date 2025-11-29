4d9c2f610a5182c92b2a16189fe96adb615c8e8b
nagham-saed122 <nagham.saed122@gmail.com>
2025-11-29 03:32:27 +0200

    Add files via upload
    Sign-UP + Login + Home Page Automation Testing

---
+++ BaseTestClass.java
@@ -0,0 +1,45 @@
+package BaseTest;
+
+import Pages.Home;
+import Pages.HomePage;
+import Pages.LoginPage;
+import Pages.SignUpPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class BaseTestClass {
+
+        protected WebDriver driver;
+        protected HomePage homePage;
+        protected SignUpPage signUpPage;
+        protected LoginPage loginPage;
+        protected Home home;
+        protected WebDriverWait wait;
+
+        @BeforeClass
+        public void setUp(){
+            driver = new FirefoxDriver();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+            driver.manage().window().maximize();
+            homePage = new HomePage(driver);
+        }
+
+        @BeforeMethod
+        public void goToHomePage(){
+            driver.get("https://demoblaze.com/");
+        }
+
+        @AfterClass
+        public void tearDown(){
+            driver.quit();
+        }
+
+    }
+
+
